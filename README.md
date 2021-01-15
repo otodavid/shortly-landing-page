ef1000da29910998998efc652c123646be5f2805
David <dieavid01@gmail.com>
2021-01-15 21:00:48 +0100

    starting files

---
+++ README.md
@@ -0,0 +1,5 @@
+A Frontend Master Challenge
+
+Shortly landing page with the functionality to shorten any url using the shrtcode API.
+
+This webpage is with Tailwindcss
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: {
+    content: ['./public/**/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          cyan: 'rgb(42, 207, 207)',
+          violet: 'rgb(59, 48, 84)'
+        },
+        secondary: 'rgb(244, 98, 98)',
+        neutral: {
+          gray: 'rgb(191, 191, 191)',
+          grayishViolet: 'rgb(158, 154, 167)',
+          blue: 'rgb(53, 50, 62)',
+          darkViolet: 'rgb(35, 33, 39)'
+        }
+      },
+      fontFamily: {
+        body: ['Poppins']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
